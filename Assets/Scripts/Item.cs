94b58b0b59d6348e6c18ffdf27baf925106d0f9b
Juan Miguel Fernandez <82823629+9ero@users.noreply.github.com>
2022-08-29 20:39:28 -0600

    Implementing Weapons

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace JM
+{
+    public class Item : ScriptableObject
+    {
+        [Header("Item information")]
+        public Sprite itemIcon;
+        public string itemName;
+       
+    }
+}
